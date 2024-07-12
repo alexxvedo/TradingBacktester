4b3767ced1423bf7f2e089ed26c34dacb0c98911
alexxvedo <alejandro.vedo@rai.usc.es>
2024-07-12 02:52:58 +0200

    Añadidos Vercel SpeedInsight y Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 import "./globals.css";
 export default function RootLayout({ children }) {
@@ -15,6 +17,8 @@ export default function RootLayout({ children }) {
           </NextUIProvider>
         </body>
       </html>
+      <SpeedInsights />
+      <Analytics />
     </ClerkProvider>
   );
 }
