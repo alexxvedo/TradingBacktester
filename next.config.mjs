0ea8d923c350b0380d91b10f5308f72ca6e1fde8
alexxvedo <alejandro.vedo@rai.usc.es>
2024-07-26 04:32:11 +0200

    Actualizado el next.config.mj5

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,9 @@
-/** @type {import('next').NextConfig} */
-
 import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 const nextConfig = {
   webpack: (config, { isServer }) => {
     if (isServer) {
-      config.plugins = [
-        ...config.plugins,
-        new PrismaPlugin({
-          paths: [
-            "./prisma/historical1",
-            "./prisma/historical2",
-            "./prisma/historical3",
-            "./prisma/historical4",
-            "./prisma/sessions",
-          ],
-        }),
-      ];
+      config.plugins = [...config.plugins, new PrismaPlugin()];
     }
 
     return config;
